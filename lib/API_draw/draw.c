665cd7c28b86209453d81d4bc571a4afc80c8af7
Erwin Blankestein <erwinb1993@gmail.com>
2018-05-20 13:53:17 +0200

    added drawrect() #27 added font pixel for prototyping

--- lib/API_draw/draw.c
+++ lib/API_draw/draw.c
@@ -1,5 +1,4 @@
 #include "draw.h"
-#include "stdlib.h"
 //--------------------------------------------------------------
 // Füllt den Screen mit einer Farbe
 //--------------------------------------------------------------
@@ -106,3 +105,45 @@ void DrawLine(int16_t x,int16_t y,int16_t x1, int16_t y1, int8_t color, int8_t w
 		}
 	}
 }
+
+
+void DrawRect(int16_t x,int16_t y,int16_t x1, int16_t y1, int8_t color)
+{
+	int yp,xp;
+
+	  for(yp=x;yp<y1;yp++) {
+	    for(xp=y;xp<x1;xp++) {
+	      UB_VGA_SetPixel(xp,yp,color);
+	    }
+	}
+}
+
+/*
+drawchar(char symbol, int16_t x,int16_t y,);
+{
+
+
+	int font_hight =15;
+	int width_pixel1 = 4;
+	int index1 = 3600;
+	int width_pixel2 = 5;
+	int index2 = 3660;
+
+
+	 uint16_t xp,yp;
+
+	  for(yp=70;yp<(font_hight+70);yp++) {
+	    for(xp=70;xp<(width_pixel2+70);xp++) {
+	      k++;
+	      UB_VGA_SetPixel(xp,yp,pixel[k+index2]);
+	    }
+	  }
+}
+*/
+
+
+
+
+
+
+
--- lib/API_draw/draw.h
+++ lib/API_draw/draw.h
@@ -1,6 +1,11 @@
 #include "api_io.h"
 
+#include "stdlib.h"
+
 void UB_VGA_FillScreen(uint8_t color);
 void DrawLine(int16_t x,int16_t y,int16_t x1, int16_t y1, int8_t color, int8_t width);
 void DrawLineNeg(int16_t x,int16_t y,int16_t x1, int16_t y1, uint8_t color, int8_t width);
 void DrawLinePos(int16_t x,int16_t y,int16_t x1, int16_t y1, uint8_t color, int8_t width);
+void DrawRect(int16_t x,int16_t y,int16_t x1, int16_t y1, int8_t color);
+void drawchar(int16_t x,int16_t y);
+
