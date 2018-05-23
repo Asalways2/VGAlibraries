ecc86f2ec5a971a6ee39762859e2ae44cc369c25
Remy van der Pol <>
2018-05-23 11:46:45 +0200

    Fix merge issue in draw.h

--- lib/API_draw/draw.h
+++ lib/API_draw/draw.h
@@ -2,6 +2,7 @@
 
 #include "stdlib.h"
 #include <math.h>
+#include "bitmap.h"
 
 void UB_VGA_FillScreen(uint8_t color);
 void DrawLine(int16_t x,int16_t y,int16_t x1, int16_t y1, int8_t color, int8_t width);
@@ -10,4 +11,4 @@ void DrawLinePos(int16_t x,int16_t y,int16_t x1, int16_t y1, uint8_t color, int8
 void DrawRect(int16_t x,int16_t y,int16_t x1, int16_t y1, int8_t color);
 void drawchar(int16_t x,int16_t y);
 void DrawEllips(int16_t x,int16_t y,int16_t x1, int16_t y1, int8_t color);
-
+void Drawbitmap(int16_t x,int16_t y,int16_t sel);
